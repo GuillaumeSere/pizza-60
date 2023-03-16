3a11027748da4d633b9831f374d3cdf10d5512cf
Guillaume Sere <guillaumesere@hotmail.fr>
2023-03-16 14:12:49 +0100

    modif front

--- api/client/src/actions/userActions.js
+++ api/client/src/actions/userActions.js
@@ -5,7 +5,7 @@ export const registerUser = (user) => async dispatch => {
     dispatch({type: 'USER_REGISTER_REQUEST'})
 
     try {
-      const response = await axios.post('https://pizza-60.vercel.app/api/users/register', user)
+      const response = await axios.post('/api/users/register', user)
         dispatch({type: 'USER_REGISTER_SUCCESSS', payload: response})
     } catch (error) {
         dispatch({type: 'USER_REGISTER_FAILED', payload: error})
@@ -17,7 +17,7 @@ export const loginUser = (user) => async dispatch => {
     dispatch({type: 'USER_LOGIN_REQUEST'})
 
     try {
-      const response = await axios.post('https://pizza-60.vercel.app/api/users/login', user)
+      const response = await axios.post('/api/users/login', user)
         dispatch({type: 'USER_LOGIN_SUCCESSS', payload: response.data})
         localStorage.setItem('currentUser', JSON.stringify(response.data))
         window.location.href= '/'
